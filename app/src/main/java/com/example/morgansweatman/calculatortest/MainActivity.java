432e13e7de7fac7e68aaa1d836bfa07a097c891c
msweatman <msweatman@stu.jsu.edu>
2019-02-21 13:01:05 -0600

    Initial Commit

--- app/src/main/java/com/example/morgansweatman/calculatortest/MainActivity.java
+++ app/src/main/java/com/example/morgansweatman/calculatortest/MainActivity.java
@@ -8,8 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private StringBuilder sb;
+    private TextView t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        t = findViewById(R.id.outputField);
     }
 
     @Override
